c908ca0c638d62bd1bec78e634f3294fdc57f393
wuxikun <xikun.wu@ushow.media>
2020-11-15 11:27:18 +0800

    xx

---
+++ base_lr/lr_02.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# @version: 1.0
+# @author: wuxikun
+# @date: '2020/11/15 11:11 AM'
+
+
+class ClassName(object):
+    def __init__(self):
+        super(ClassName, self).__init__
+
+
+if __name__ == "__main__":
+    pass
---
+++ estimator_d/lr_estimator.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# @version: 1.0
+# @author: wuxikun
+# @date: '2020/11/15 10:36 AM'
+
+
+class ClassName(object):
+    def __init__(self):
+        super(ClassName, self).__init__
+
+
+if __name__ == "__main__":
+    pass
