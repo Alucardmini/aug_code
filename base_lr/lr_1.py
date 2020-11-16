64bcbb2a5acb97ab207505a11586c0291f0dbfb4
wuxikun <xikun.wu@ushow.media>
2020-11-16 23:00:08 +0800

    xx

--- base_lr/lr_1.py
+++ base_lr/lr_1.py
@@ -39,33 +39,44 @@ def train(sess, xs, ys, steps, x, y, train_op, loss):
 
 if __name__ == '__main__':
     pb_file_path = "./model/model_builder/"
+    ckpt_path = "./model/model_ckpt/model"
     # x, y, loss, train_op, w, b = lr_model()
     # init = tf.global_variables_initializer()
     # xs = np.random.randint(0, 100, 1000)
     # ys = xs * 3 + 0.25
     # with tf.Session() as sess:
     #    train(sess, xs, ys, 10000, x, y, train_op, loss)
-    #    with tf.gfile.FastGFile(pb_file_path + 'model.pb', mode='wb') as f:
-    #        constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph_def, ['w', 'b', 'logit'])
-    #        f.write(constant_graph.SerializeToString())
+    #    # with tf.gfile.FastGFile(pb_file_path + 'model.pb', mode='wb') as f:
+    #    #     constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph_def, ['w', 'b', 'logit'])
+    #    #     f.write(constant_graph.SerializeToString())
     #
-    #    m = sess.graph.get_tensor_by_name('w:0')
-    #    print("===", sess.run(m))
+    #    save_path = saver.save(sess, ckpt_path, global_step=10000)
 
-    sess = tf.Session()
 
-    with tf.gfile.FastGFile(pb_file_path + 'model.pb', 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-        sess.graph.as_default()
-        tf.import_graph_def(graph_def, name='')  # 导入计算图
-    sess.run(tf.global_variables_initializer())
 
-    input_x = sess.graph.get_tensor_by_name('x:0')
-    logit = sess.graph.get_tensor_by_name('logit:0')
-    ret = sess.run(logit, feed_dict={input_x: 5, })
-    print(ret)
+    # sess = tf.Session()
+    # sess.run(tf.global_variables_initializer())
+    #
+    # with tf.gfile.FastGFile(pb_file_path + 'model.pb', 'rb') as f:
+    #     graph_def = tf.GraphDef()
+    #     graph_def.ParseFromString(f.read())
+    #     sess.graph.as_default()
+    #     tf.import_graph_def(graph_def, name='')  # 导入计算图
+    #
+    # input_x = sess.graph.get_tensor_by_name('x:0')
+    # logit = sess.graph.get_tensor_by_name('logit:0')
+    # ret = sess.run(logit, feed_dict={input_x: 5, })
+    # print(ret)
+    #
+    # print(sess.run('w:0'))
+    # print(sess.run('b:0'))
 
-    print(sess.run('w:0'))
-    print(sess.run('b:0'))
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        graph = tf.train.import_meta_graph(ckpt_path + "-10000.meta")
+        graph.restore(sess, tf.train.latest_checkpoint( "./model/model_ckpt/" ))
+        print(sess.run('w:0'))
+        print(sess.run('b:0'))
 
