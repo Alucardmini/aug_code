f9d882af43c994a77a2a6b1fc234a2cc6e477d4e
wuxikun <xikun.wu@ushow.media>
2021-08-18 21:53:41 +0800

    dev

--- estimator_d/lr_estimator.py
+++ estimator_d/lr_estimator.py
@@ -7,35 +7,84 @@
 import tensorflow as tf
 import numpy as np
 
-def lr_model():
+x_train = np.random.random((100, 1))
+y_train = x_train* 3 + 0.7
+features = {
+    "x": x_train
+}
 
-    x = tf.placeholder(dtype=tf.float32, name="x")
-    y = tf.placeholder(dtype=tf.float32, name="y")
+feature_cols = tf.feature_column.numeric_column("x", default_value=0.0)
 
-    # init = tf.random_normal_initializer(0.0, 1)
-    # w = tf.get_variable("w", shape=[1], initializer=init)
-    # b = tf.get_variable("b", shape=[1], initializer=init)
+def model_fn(features, labels, mode, params):
 
-    w = tf.Variable(tf.zeros([1]))
-    b = tf.Variable(tf.zeros([1]))
+    x = features['x']
 
-    y_pred = w * x + b
-    loss = tf.reduce_mean(tf.sqrt(y_pred*y_pred - y*y))
-    opt = tf.train.GradientDescentOptimizer(0.01)
-    train_op = opt.minimize(loss)
-
-    return x, y, loss, train_op
+    w = tf.get_variable(name="w", shape=[1], initializer=tf.zeros_initializer)
+    b = tf.get_variable(name="b", shape=[1], initializer=tf.zeros_initializer)
+    y_pred = w*x + b
 
 
-if __name__ == "__main__":
+    loss = tf.reduce_mean(tf.square(y_pred - labels))
+    train_op = tf.train.GradientDescentOptimizer(learning_rate=params['lr_rate']).minimize(loss)
 
-    x_data = np.random.randint(0, 100, 100).astype(np.float32)
-    y_data = 2*x_data + 0.05
-    init = tf.compat.v1.global_variables_initializer()
-    x, y, loss_op, train_op = lr_model()
 
-    with tf.Session() as sess:
-        sess.run(init)
-        loss, _ = sess.run([loss_op, train_op], feed_dict={x: x_data, y: y_data})
+    return tf.estimator.EstimatorSpec(mode=mode, train_op=train_op, loss=loss)
+
+if __name__ == '__main__':
+    x_train = np.random.random((1000, 1)).astype(np.float32)
+    y_train = x_train * 3 + 0.7
+
+    print(x_train.dtype)
+
+
+    model_params = {"lr_rate": 0.01}
+    model = tf.estimator.Estimator(model_fn=model_fn, params=model_params, model_dir="model/lr")
+
+
+
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": x_train},
+                                                    y=y_train,
+                                                    num_epochs=10,
+                                                    batch_size=8,
+                                                    shuffle=True)
+
+    model.train(input_fn=train_input_fn, steps=1000)
+
+
+    for name in  model.get_variable_names():
+        print(name, model.get_variable_value(name=name))
+
+
+
+
+
+
+
+
+# x = tf.placeholder(dtype=tf.float32, shape=[None, 1], name="x")
+# y = tf.placeholder(dtype=tf.float32, shape=[None, 1], name="x")
+# w = tf.get_variable(name="w", shape=[1], initializer=tf.zeros_initializer)
+# b = tf.get_variable(name="b", shape=[1], initializer=tf.zeros_initializer)
+# y_pred = w*x + b
+# loss = tf.reduce_mean(tf.square(y_pred - y))
+# train_op = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
+# x_train = np.random.random((100, 1))
+# y_train = x_train* 3 + 0.7
+# print(x_train.shape)
+# print(y_train.shape)
+#
+# with tf.Session() as sess:
+#
+#     sess.run(tf.global_variables_initializer())
+#
+#     for i in range(1000):
+#
+#        _, loss_val = sess.run([train_op, loss], feed_dict={x: x_train, y: y_train})
+#
+#        if i % 100 == 0:
+#            print(loss_val)
+
+
+
 
 
--- tf_record_lr/record_demo.py
+++ tf_record_lr/record_demo.py
@@ -5,30 +5,70 @@
 
 import tensorflow as tf
 import numpy as np
-path = "./tf_record"
-writer = tf.python_io.TFRecordWriter(path)
+from PIL import Image
 
-tfrecords_filename = 'train.tfrecords'
-write = tf.python_io.TFRecordWriter(tfrecords_filename)
 
-for i in range(100):
 
-  img_raw = np.random.random_integers(0,255,size=(30,30))  # Create image size 30*30
+def write():
+    path = "./tf_record"
+    tf.python_io.TFRecordWriter(path)
 
-  img_raw = img_raw.tostring()  # float data to bytes
+    tfrecords_filename = 'train.tfrecords'
+    write = tf.python_io.TFRecordWriter(tfrecords_filename)
 
-  example = tf.train.Example(features=tf.train.Features(
+    for i in range(100):
 
-      feature = {
+      img_raw = np.random.random_integers(0,255,size=(30,30))  # Create image size 30*30
 
-          'label':tf.train.Feature(int64_list=tf.train.Int64List(value=[i])),
+      img_raw = img_raw.tostring()  # float data to bytes
 
-          'img_raw':tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+      example = tf.train.Example(features=tf.train.Features(
 
-      }
+          feature = {
 
-  ))
+              'label':tf.train.Feature(int64_list=tf.train.Int64List(value=[i])),
 
-  write.write(example.SerializeToString())
+              'img_raw':tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
 
-write.close()
+          }
+
+      ))
+
+      write.write(example.SerializeToString())
+
+    write.close()
+
+def read():
+    input_files = ['/Users/wuxikun/Documents/LetsAug/tf_record_lr/train.tfrecords']
+
+    dataset = tf.data.TFRecordDataset(input_files)
+
+    def parser(record):
+        features = tf.parse_single_example(
+            record,
+            features={  # 和原来生成tfrecord时候要对应相同的
+                'label': tf.FixedLenFeature([], tf.int64),
+                'img_raw': tf.FixedLenFeature([], tf.string)
+
+            }
+        )
+        return features["label"], features['img_raw']
+
+    dataset = dataset.map(parser)  # 接受的参数是一个函数
+
+    iterator = dataset.make_initializable_iterator()
+    return iterator, iterator.get_next()
+
+
+if __name__ == '__main__':
+    iterator, next_elem = read()
+
+    with tf.Session() as sess:
+
+        sess.run(iterator.initializer)
+
+        for i in range(10):
+
+            image = tf.decode_raw(next_elem[1], tf.uint8)
+            print(sess.run(next_elem[0]))
+            print(sess.run(image))
